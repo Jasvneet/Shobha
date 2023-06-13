95e8287761609890be83fe072d1aae2b4722323c
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-06-13 10:20:40 -0700

    backened phase 1

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "byebug", platforms: %i[ mri mingw x64_mingw ]
+  gem "faker"
 end
 
 group :development do
@@ -54,5 +55,6 @@ group :development do
   gem "pry-rails"
   gem "better_errors"
   gem "binding_of_caller"
+  gem "sassc"
 end
 
---
+++ app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :firstname, :lastname, presence: true
+  validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  has_secure_password
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email )
+
+    if user && user.authenticate(password)
+      return user 
+    end 
+    nil
+  end  
+
+  def reset_session_token!
+		self.session_token = generate_unique_session_token
+		self.save!
+		return self.session_token
+	end
+
+  
+  private
+
+  def generate_unique_session_token
+		loop do
+			session_token = SecureRandom::urlsafe_base64(16)
+			return session_token unless User.exists?(session_token: session_token)
+		end
+	end
+
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end 
+
+
+end
