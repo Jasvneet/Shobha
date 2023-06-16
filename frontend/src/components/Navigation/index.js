7581cdabfebe9db3475bbe0fc8c652e8c54b924c
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-06-15 22:03:55 -0700

    icon css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,7 +4,8 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
-
+import { FaRegComment, FaComment, FaRegHeart, FaHeart } from "react-icons/fa";
+import {BsBasket2, BsBasket2Fill} from "react-icons/bs"
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
@@ -54,17 +55,32 @@ function Navigation() {
         {sessionLinks}
       </li>
 
-      <li>
-      <i className='far fa-comment fa-flip-horizontal'></i>
+      <li id='chat-icon-container'>
+      <div id="icon-container" className='dropdown'>
+        <span id="original-icon" class="icon"><FaRegComment /></span>
+        <span id="replacement-icon" class="icon"><FaComment /></span>
+        <div class="dropdown-content">
+        <p>Live Chat</p>
+      </div>
+      </div>
+      </li>
+        
+      <li id='heart-icon-container'>
+      <div id="icon-container">
+        <span id="original-icon" class="icon"><FaRegHeart /></span>
+        <span id="replacement-icon" class="icon"><FaHeart /></span>
+      </div>
       </li>
 
       <li>
-      <i class="far fa-sharp fa-light fa-heart"></i>
+        <div>
+        <div id="icon-container">
+        <span id="original-icon" class="icon"><BsBasket2 /></span>
+        <span id="replacement-icon" class="icon"><BsBasket2Fill /></span>
+      </div>
+        </div>
       </li>
       
-      <li>
-      <i class="far fa-solid fa-basket-shopping"></i>
-      </li>
 
 
     </ul>
