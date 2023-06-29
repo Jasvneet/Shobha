02b51609e7d6e851468ff41766041c5d85a47f6a
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-06-29 09:18:18 -0700

    styling home

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -27,6 +27,39 @@ const Home = () => {
         <div className='divider-home'></div>
         <div className='featured-categories-container'>
           <ul className='featured-categories'>
+              <div className='featured-heading-container'>
+                <h2>Featured Categories</h2>
+                <div className='featured-subheading'>
+                  Shop what's popular now.
+                </div>
+
+              </div>
+              <li className='category'>
+                <a href="#" className='category-details'>
+                  <h5>Foundation</h5>
+                  <div className='category-img-container'>
+                  <img src="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_samples_32_us_ca_rwd_slice.png?imwidth=48" srcset="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_samples_32_us_ca_rwd_slice.png?imwidth=48 1x, https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_samples_32_us_ca_rwd_slice.png?imwidth=96 2x" loading="lazy" role="presentation" alt="" class="css-z3vs2m eanm77i0" data-comp="Image StyledComponent BaseComponent " />
+                  </div>
+                </a>
+              </li>
+
+              <li className='category'>
+                <a href="#" className='category-details'>
+                  <h5>Moisturizers</h5>
+                  <div className='category-img-container'>
+                  <img src="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_skincare_cleanser_moisturizer_us_ca_rwd_slice.jpg?imwidth=48" srcset="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_skincare_cleanser_moisturizer_us_ca_rwd_slice.jpg?imwidth=48 1x, https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_skincare_cleanser_moisturizer_us_ca_rwd_slice.jpg?imwidth=96 2x" loading="lazy" role="presentation" alt="" class="css-z3vs2m eanm77i0"/>
+                  </div>
+                </a>
+              </li>
+              
+              <li className='category'>
+                <a href="#" className='category-details'>
+                  <h5>Hair Styling & Treatments</h5>
+                  <div className='category-img-container'>
+                  <img src="https://www.sephora.com/contentimages/homepage/091322/Homepage/RWD/hair%20dryer.JPG?imwidth=48" srcset="https://www.sephora.com/contentimages/homepage/091322/Homepage/RWD/hair%20dryer.JPG?imwidth=48 1x, https://www.sephora.com/contentimages/homepage/091322/Homepage/RWD/hair%20dryer.JPG?imwidth=96 2x" loading="lazy" role="presentation" alt="" class="css-z3vs2m eanm77i0" data-comp="Image StyledComponent BaseComponent "/>
+                  </div>
+                </a>
+              </li>
 
               <li className='category'>
                 <a href="#" className='category-details'>
@@ -36,6 +69,24 @@ const Home = () => {
                   </div>
                 </a>
               </li>
+              <li className='category'>
+                <a href="#" className='category-details'>
+                  <h5>Minis</h5>
+                  <div className='category-img-container'>
+                  <img src="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_minis_us_ca_rwd_slice.png?imwidth=48" srcset="https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_minis_us_ca_rwd_slice.png?imwidth=48 1x, https://www.sephora.com/contentimages/homepage/060222/Homepage/RWD/CategoryTiles/homepage_featured_category_tile_minis_us_ca_rwd_slice.png?imwidth=96 2x" loading="lazy" role="presentation" alt="" class="css-z3vs2m eanm77i0" data-comp="Image StyledComponent BaseComponent "/>
+                  </div>
+                </a>
+              </li>
+
+              <li className='category'>
+                <a href="#" className='category-details'>
+                  <h5>Gifts</h5>
+                  <div className='category-img-container'>
+                    <img src="https://www.sephora.com/contentimages/homepage/092022/Homepage/RWD/gifts%2032@3x.png?imwidth=48" />
+                  </div>
+                </a>
+              </li>
+            
 
           </ul>
         </div>
